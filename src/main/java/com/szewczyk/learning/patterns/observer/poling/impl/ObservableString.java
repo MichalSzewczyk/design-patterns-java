e1ee4be7a42bde1f1a54ed47c985545ebbcf0b7d
mszewczyk <szewczykm94@gmail.com>
2018-06-30 12:54:53 +0200

    Implemented poling observer example

--- src/main/java/com/szewczyk/learning/patterns/observer/poling/impl/ObservableString.java
+++ src/main/java/com/szewczyk/learning/patterns/observer/poling/impl/ObservableString.java
@@ -3,15 +3,29 @@ package com.szewczyk.learning.patterns.observer.poling.impl;
 import com.szewczyk.learning.patterns.observer.poling.Observable;
 import com.szewczyk.learning.patterns.observer.poling.Observer;
 
-public class ObservableString implements Observable<String> {
+import java.util.LinkedList;
+import java.util.List;
+
+public class ObservableString implements Observable<String>, Runnable {
+    private final List<Observer<String>> observers;
+
+    public ObservableString() {
+        observers = new LinkedList<>();
+    }
+
 
     @Override
     public void subscribe(Observer<String> observer) {
-
+        observers.add(observer);
     }
 
     @Override
     public String getActualValue() {
-        return null;
+        return "Always the same.";
+    }
+
+    @Override
+    public void run() {
+        observers.forEach(Observer::notifyChange);
     }
 }
--- src/main/java/com/szewczyk/learning/patterns/observer/poling/impl/StringObserver.java
+++ src/main/java/com/szewczyk/learning/patterns/observer/poling/impl/StringObserver.java
@@ -1,11 +1,18 @@
 package com.szewczyk.learning.patterns.observer.poling.impl;
 
+import com.szewczyk.learning.patterns.observer.poling.Observable;
 import com.szewczyk.learning.patterns.observer.poling.Observer;
 
 public class StringObserver implements Observer<String> {
+    private final Observable<String> stringObservable;
+
+    public StringObserver(Observable<String> stringObservable) {
+        this.stringObservable = stringObservable;
+        this.stringObservable.subscribe(this);
+    }
 
     @Override
     public void notifyChange() {
-
+        System.out.println("After change: " + stringObservable.getActualValue());
     }
 }
